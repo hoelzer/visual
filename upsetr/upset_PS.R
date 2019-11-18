37f2ede595e1d1cb5a8550732d5fd8361420c54a
hoelzer <hoelzer.martin@gmail.com>
2019-11-18 17:40:14 +0000

    add test script trice

--- upsetr/upset_PS.R
+++ upsetr/upset_PS.R
@@ -31,7 +31,7 @@ svg(filename="upsetr.svg",
     width=14, 
     height=12, 
     pointsize=12)
-upset(fromList(sets), nsets = 20, nintersects = 40, sets = c("Ctr","Cmu","Csu","Cpn","Cpe","Cib","Cga","Cav","Cfe","Cca","Cps","Cab"), # sets=names(sets)
+upset(fromList(sets), nsets = 20, nintersects = 40, sets = names(sets),# sets = c("Ctr","Cmu","Csu","Cpn","Cpe","Cib","Cga","Cav","Cfe","Cca","Cps","Cab"),
     mainbar.y.label = "No. of common genes", sets.x.label = "No. of annotated genes", 
     order.by = "freq", sets.bar.color = "#56B4E9", keep.order = T, 
     text.scale = 1.4, point.size = 2.6, line.size = 0.8,
