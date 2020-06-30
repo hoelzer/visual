f8f8eee59a09fd1b879f29ceea1570fa1b3f4885
hoelzer <hoelzer.martin@gmail.com>
2020-06-30 15:24:13 +0200

    renaming balloon labels

--- balloon/balloon.R
+++ balloon/balloon.R
@@ -7,20 +7,20 @@ library(svglite)
 
 args = commandArgs(trailingOnly = TRUE)
 
-cols <- c('name', 'vore', 'variable', 'value')
+cols <- c('name', 'rank', 'variable', 'number')
 df.virify <- as_tibble(read.table(args[1]))
 names(df.virify) <- cols
 
-# add a "row" column which will be the y position in the plot: group by vore and name, then set "row" as group index
+# add a "row" column which will be the y position in the plot: group by rank and name, then set "row" as group index
 df.virify <- df.virify %>% mutate(row = group_indices_(df.virify, .dots=c('name')))
 
 # define order of columns in the plot, sort df.virify
 order <- c("order", "family", "subfamily", "genus")
 df.virify <- df.virify %>% 
-    mutate(vore =  factor(vore, levels = order)) %>%
-    arrange(vore)
+    mutate(rank =  factor(rank, levels = order)) %>%
+    arrange(rank)
 
-df.virify <- df.virify %>% unite(vore, variable, col = "col_order", remove = F)
+df.virify <- df.virify %>% unite(rank, variable, col = "col_order", remove = F)
 col_levels <- unique(df.virify$col_order)
 
 # ask trice if something is unclear ;D
@@ -33,9 +33,9 @@ vars_x_axis <- (df.virify %>% arrange(col) %>% select(variable) %>% distinct())$
 # get character vector of observation names for the y axis. again, the order is important but "df" is already ordered
 names_y_axis <- (df.virify %>% group_by(row) %>% distinct(name) %>% ungroup() %>% arrange(row) %>% select(name))$name
 
-plot <- ggplot(df.virify, aes(x=factor(col), y=factor(row), color=vore, size=value, alpha=value)) +
+plot <- ggplot(df.virify, aes(x=factor(col), y=factor(row), color=rank, size=number, alpha=number)) +
   geom_point() +  # plot as points
-  geom_text(aes(label=value, x=col + 0.25), alpha=1.0, size=3) +   # display the value next to the "balloons"
+  geom_text(aes(label=number, x=col + 0.25), alpha=1.0, size=3) +   # display the number next to the "balloons"
   scale_alpha_continuous(range=c(0.3, 0.7)) +
   scale_size_area(max_size = 5) +
   scale_x_discrete(breaks=1:length(vars_x_axis), labels=vars_x_axis, position='top') +   # set the labels on the X axis
